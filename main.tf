7d78c0bdfba01722bad9f21e7c20c5a934c2abad
zablon <zablonoigo51@gmail.com>
2024-12-21 19:14:43 +0300

    Add vpc id and AZ for the subnet

--- main.tf
+++ main.tf
@@ -4,4 +4,10 @@ data "aws_vpc" "vpc" {
   default = true
 }
 data "aws_subnet" "subnet1" {
+    vpc_id = data.aws_vpc.vpc.id 
+    availability_zone = "us-east-1a"
+}
+data "aws_subnet" "subnet2" {
+    vpc_id = data.aws_vpc.vpc.id
+    availability_zone = "us-east-1b"
 }
